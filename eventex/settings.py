8dc7e37a39d8bbdcb081b7dadf8783284e16169c
André Simões <alsimoes@gmail.com>
2012-09-24 19:00:19 -0300

    Configuranto o deploy.

--- eventex/settings.py
+++ eventex/settings.py
@@ -1,5 +1,6 @@
 # Django settings for eventex project.
 from unipath import Path
+import dj_database_url
 
 PROJECT_DIR = Path(__file__).parent
 
@@ -13,14 +14,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': PROJECT_DIR.child('database.db'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default='sqlite:///' + PROJECT_DIR.child('database.db'))
 }
 
 # Local time zone for this installation. Choices can be found here:
@@ -48,18 +42,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = PROJECT_DIR.child('media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_DIR.child('public')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
