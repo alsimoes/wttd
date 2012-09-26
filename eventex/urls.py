34df015a31a679f8d12fcaaa14826761e11404e8
André Simões <alsimoes@gmail.com>
2012-09-25 23:50:36 -0300

    urls.py

--- eventex/urls.py
+++ eventex/urls.py
@@ -20,5 +20,11 @@ urlpatterns = patterns('eventex.core.views',
 
 if settings.DEBUG is False:
     urlpatterns += patterns('',
-        url(r'ˆstatic/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT}),
-        )
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
+"""
+if settings.DEBUG is False:   #if DEBUG is True it will be served automatically
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
+"""
